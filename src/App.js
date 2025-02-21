91bc601c14fd27702044f12dbf85a2f49e75812c
Tori <ttang1135@naver.com>
2025-02-21 15:46:36 +0900

    feat: 임시 로그인 페이지 연결(네이버 로그인)

--- src/App.js
+++ src/App.js
@@ -9,22 +9,77 @@ import Inquiries from './pages/Inquiries.jsx';
 import Notices from './pages/Notices.jsx';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from './styles/FontStyle/GlobalStyle.js';
+import PrivateRoute from './components/PrivateRoute.js';
+import LoginPage from './pages/Login/Login.jsx';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/reports" element={<Reports />} />
-            <Route path="/inquiries" element={<Inquiries />} />
-            <Route path="/notices" element={<Notices />} />
-          </Routes>
-        </Layout>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <Home />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/users"
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <Users />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/posts"
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <Posts />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/reports"
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <Reports />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/inquiries"
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <Inquiries />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/notices"
+            element={
+              <PrivateRoute>
+                <Layout>
+                  <Notices />
+                </Layout>
+              </PrivateRoute>
+            }
+          />
+        </Routes>
       </Router>
     </ThemeProvider>
   );
